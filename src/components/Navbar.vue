01b4d485ddce46bf64159532f02ef8723b2e9d86
Ильичёв Иван Сергеевич <iilichev@corp.local>
2021-08-23 18:28:45 +0300

    [+] navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-container>
+        <b-navbar-brand href="#">Mealzers</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                placeholder="Search for a meal..."
+                v-model="meal"
+              ></b-form-input>
+
+              <b-button
+                size="sm"
+                class="my-2 my-sm-0"
+                type="submit"
+                @click.prevent="getMeal"
+              ></b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown right>
+              <template slot="button-content">
+                <em>User</em>
+              </template>
+
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      meal: ''
+    }
+  },
+  methods: {
+    getMeal() {
+      console.log('get meal')
+    }
+  }
+}
+</script>
